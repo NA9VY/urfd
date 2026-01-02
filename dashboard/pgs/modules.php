a05bc7171e36f0f024a793e9b6906769943d01fc
NA9VY <13339888+NA9VY@users.noreply.github.com>
2026-01-02 15:18:34 -0500

    Enhance modules.php with module descriptions
    Added detailed information about modules and their usage in the reflector.

--- dashboard/pgs/modules.php
+++ dashboard/pgs/modules.php
@@ -49,3 +49,20 @@ for ($i = 1; $i <= $NumberOfModules; $i++) {
 ?>
 
 </table>
+
+<p><b>What are Modules?</b></p>
+<br>
+<p>Modules could also be called chat rooms. They are different areas within a reflector to have conversations that are separate from other modules.
+You might also say the main module on a reflector, often Module A, is the calling module and once you've
+established contact with the person you are calling you can both move to a different module to continue the conversation.</p>
+<br>
+<p>Reflector operators have to option of assigning names or topic to the modules they have on their reflector. For example, some operators may dedicated
+modules to AMSAT or POTA conversations while others call the modules Chat Room B and Chat Room C.</p>
+<br>
+<p>Depending on the configuration of the reflector, one or more modules can also be transcoded to support multiple modes (D-Star, YSF, DMR, etc.) of
+operation at the same time. Modules that are not transcoded can only support one mode at a time.</p>
+<br>
+<p>How a users moves between modules depends on the mode they are using. YSF users transmit with the modules assigned DG-ID to
+access the modules they would like to use. DMR users make a private
+call to the modules assigned talkgroup and then move back to group call talk group to start a conversation.
+D-Star users use the modules assigned URCALL to move to different modules.<p>
